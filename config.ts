37ce14aef52ae2becad94aa4580350c53f1f70b5
leon <maxime.noel@epitech.eu>
2023-02-14 14:27:50 +0100

    feat: get content on wikipedia page

---
+++ config.ts
@@ -0,0 +1,4 @@
+export default {
+    login : "addresse@gmail.com",
+    password : "mptdepasse"
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+import account from '../config.js';
 
 async function scrape() {
     let browser = await puppeteer.launch({
@@ -6,12 +7,35 @@ async function scrape() {
         args: ["--disable-setuid-sandbox"],
         'ignoreHTTPSErrors': true
     });
+    
     let page = await browser.newPage();
-    await page.goto("https://www.instagram.com/?hl=fr");
+    // await page.goto("https://www.instagram.com/?hl=fr");
+    await page.goto("https://en.wikipedia.org/wiki/Puppeteer");
 
-    const button = '[class="_a9-- _a9_1"]';
-    await page.waitForSelector(button);;
-    await page.click(button);
+    // const cookieButton = '[class="_a9-- _a9_1"]';                    //Instagram
+    // const cookieButton = '[class="yt-spec-button-shape-next yt-spec-button-shape-next--filled yt-spec-button-shape-next--call-to-action yt-spec-button-shape-next--size-m "]';  // youtube
+    // await page.waitForSelector(cookieButton);
+    // await page.click(cookieButton);
+    
+    let element = await page.$('[class="hatnote navigation-not-searchable"]');
+    let value = await page.evaluate(el => el.textContent, element);
+    console.log((value));
+    
+    // await page.waitForTimeout(1000);
+
+
+    // let loginInput:string = '[type="text"]';
+    // let passwordInput:string = '[type="password"]';
+    // let submitButton = '[class="_acan _acap _acas _aj1-"]';
+    // await page.waitForSelector(loginInput);
+    // await page.waitForSelector(passwordInput);
+    // await page.waitForSelector(submitButton);
+
+    // await page.type(loginInput, account.login);
+    // await page.type(passwordInput, account.password);
+    // await page.click(submitButton);
+
+
 
     // await page.screenshot({path :'exemple.png'});
     await browser.close();
